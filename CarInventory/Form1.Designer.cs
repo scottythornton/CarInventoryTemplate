a5dc40630ca0aef009400d5a9f1f0a5ff5ae007a
Scotty Thornton <scotty.thornton17@outlook.com>
2020-05-20 16:35:34 -0400

    finished

--- CarInventory/Form1.Designer.cs
+++ CarInventory/Form1.Designer.cs
@@ -38,41 +38,47 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // yearInput
             // 
-            this.yearInput.Location = new System.Drawing.Point(97, 33);
+            this.yearInput.Location = new System.Drawing.Point(194, 63);
+            this.yearInput.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.yearInput.Name = "yearInput";
-            this.yearInput.Size = new System.Drawing.Size(127, 20);
+            this.yearInput.Size = new System.Drawing.Size(250, 31);
             this.yearInput.TabIndex = 0;
             // 
             // makeInput
             // 
-            this.makeInput.Location = new System.Drawing.Point(97, 59);
+            this.makeInput.Location = new System.Drawing.Point(194, 113);
+            this.makeInput.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.makeInput.Name = "makeInput";
-            this.makeInput.Size = new System.Drawing.Size(127, 20);
+            this.makeInput.Size = new System.Drawing.Size(250, 31);
             this.makeInput.TabIndex = 1;
             // 
             // colourInput
             // 
-            this.colourInput.Location = new System.Drawing.Point(97, 85);
+            this.colourInput.Location = new System.Drawing.Point(194, 163);
+            this.colourInput.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.colourInput.Name = "colourInput";
-            this.colourInput.Size = new System.Drawing.Size(127, 20);
+            this.colourInput.Size = new System.Drawing.Size(250, 31);
             this.colourInput.TabIndex = 2;
             // 
             // mileageInput
             // 
-            this.mileageInput.Location = new System.Drawing.Point(97, 111);
+            this.mileageInput.Location = new System.Drawing.Point(194, 213);
+            this.mileageInput.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.mileageInput.Name = "mileageInput";
-            this.mileageInput.Size = new System.Drawing.Size(127, 20);
+            this.mileageInput.Size = new System.Drawing.Size(250, 31);
             this.mileageInput.TabIndex = 3;
             // 
             // addButton
             // 
-            this.addButton.Location = new System.Drawing.Point(97, 144);
+            this.addButton.Location = new System.Drawing.Point(194, 277);
+            this.addButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(64, 23);
+            this.addButton.Size = new System.Drawing.Size(128, 44);
             this.addButton.TabIndex = 4;
             this.addButton.Text = "Add";
             this.addButton.UseVisualStyleBackColor = true;
@@ -81,53 +87,69 @@
             // outputLabel
             // 
             this.outputLabel.BackColor = System.Drawing.Color.White;
-            this.outputLabel.Location = new System.Drawing.Point(37, 192);
+            this.outputLabel.Location = new System.Drawing.Point(74, 369);
+            this.outputLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.outputLabel.Name = "outputLabel";
-            this.outputLabel.Size = new System.Drawing.Size(300, 138);
+            this.outputLabel.Size = new System.Drawing.Size(600, 265);
             this.outputLabel.TabIndex = 5;
             this.outputLabel.Text = "label1";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(37, 36);
+            this.label1.Location = new System.Drawing.Point(74, 69);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.Size = new System.Drawing.Size(58, 25);
             this.label1.TabIndex = 6;
             this.label1.Text = "Year";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(37, 62);
+            this.label2.Location = new System.Drawing.Point(74, 119);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.Size = new System.Drawing.Size(65, 25);
             this.label2.TabIndex = 7;
             this.label2.Text = "Make";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(37, 88);
+            this.label3.Location = new System.Drawing.Point(74, 169);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.Size = new System.Drawing.Size(75, 25);
             this.label3.TabIndex = 8;
             this.label3.Text = "Colour";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(37, 114);
+            this.label4.Location = new System.Drawing.Point(74, 219);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.Size = new System.Drawing.Size(88, 25);
             this.label4.TabIndex = 9;
             this.label4.Text = "Mileage";
             // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(496, 277);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(128, 42);
+            this.exitButton.TabIndex = 10;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(379, 366);
+            this.ClientSize = new System.Drawing.Size(758, 704);
+            this.Controls.Add(this.exitButton);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -138,6 +160,7 @@
             this.Controls.Add(this.colourInput);
             this.Controls.Add(this.makeInput);
             this.Controls.Add(this.yearInput);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Car Inventory";
             this.ResumeLayout(false);
@@ -157,6 +180,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button exitButton;
     }
 }
 
--- CarInventory/Form1.cs
+++ CarInventory/Form1.cs
@@ -11,7 +11,8 @@ namespace CarInventory
 {
     public partial class Form1 : Form
     {
-
+        List<Car> carsList = new List<Car>();
+        //List<save> saveList = new List<save>();
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +20,33 @@ namespace CarInventory
 
         private void addButton_Click(object sender, EventArgs e)
         {
+            string make, colour, mileage, year;
 
+            year = yearInput.Text;
+            make = makeInput.Text;
+            mileage = mileageInput.Text;
+            colour = colourInput.Text;
+
+            Car c = new Car(year, make, colour, mileage);
+
+            carsList.Add(c);
+
+            outputLabel.Text = "";
+
+            //for (int i = 0; i < carsList.Count; i++)
+            //{
+            //    outputLabel.Text += carsList[i].year + " " + carsList[i].make + " " + carsList[i].colour + " " + carsList[i].mileage + "\n";
+            //}
+
+            foreach(Car car in carsList)
+            {
+                outputLabel.Text = c.year + " " + c.make + " " + c.colour + " " + c.mileage + "\n";
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
